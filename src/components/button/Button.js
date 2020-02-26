e4bb5c7d1e1ebeabc1810288aa68e44408fb66da
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-26 15:05:24 -0500

    Converts the ES6 syntax of 'Button.js' to React hooks syntax

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class Button extends Component {
-	state = {
-		buttonText: 'Click me, please'
-	};
+export default function Button() {
+	const [buttonText, setButtonText] = useState('Click me, please');
 
-	handleClick = () => {
-		this.setState(() => {
-			return {
-				buttonText: 'Thanks, been clicked!'
-			};
-		});
-	};
-
-	render() {
-		const { buttonText } = this.state;
-		return <button onClick={this.handleClick}>{buttonText}</button>;
-	}
+	return (
+		<button onClick={() => setButtonText('Thanks, been clicked!')}>
+			{buttonText}
+		</button>
+	);
 }
