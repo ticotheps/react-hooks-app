15aa79d0c0e52588bcf511e83356947a9de4b004
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-26 15:09:09 -0500

    Provides example of how to refactor 'Button.js' with use of regular function being passed to 'onClick' handler instead of using an arrow function

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,9 +3,15 @@ import React, { useState } from 'react';
 export default function Button() {
 	const [buttonText, setButtonText] = useState('Click me, please');
 
-	return (
-		<button onClick={() => setButtonText('Thanks, been clicked!')}>
-			{buttonText}
-		</button>
-	);
+	// return (
+	// 	<button onClick={() => setButtonText('Thanks, been clicked!')}>
+	// 		{buttonText}
+	// 	</button>
+	// );
+
+	function handleClick() {
+		return setButtonText('Thanks, been clicked!');
+	}
+
+	return <button onClick={handleClick}>{buttonText}</button>;
 }
